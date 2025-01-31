70b4c412c527efdc52ff22c8f895ecce53152757
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 10:50:19 +0530

    Create pp6.py

---
+++ lab 2/pp6.py
@@ -0,0 +1,5 @@
+# This program demonstrates input and output.
+name = input("Enter your name: ") # Prompting for input
+print("Hello, " + name + "!") # Outputting the input value
+age = int(input("Enter your age: ")) # Type conversion to integer
+print("Next year, you will be " + str(age + 1) + " years old.") #string catenation
