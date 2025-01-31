74ce369b2945d346773705adca4723235771850b
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 10:48:31 +0530

    Create pp2.py

---
+++ lab 2/pp2.py
@@ -0,0 +1,9 @@
+# Input two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+# Calculate sum and average
+sum = num1 + num2
+average = sum / 2
+# Display results
+print(f"Sum of the two numbers is: {sum}")
+print(f"Average of the two numbers is: {average}")
