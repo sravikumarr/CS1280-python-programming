b6963af4396d1d2f8ea1bd8fa3821f46c6cbb7c6
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 11:08:16 +0530

    Update pp4.py

--- lab 2/pp4.py
+++ lab 2/pp4.py
@@ -1,11 +1,9 @@
-x = "I am in RVU"  # single quote
-y = "What is your name"  # double quotes
-z = """Hello world"""  # triple quote
-print(x, y, sep=";")
-print(z, x, sep="=")
-"""
-# Configuring the output record separator:
-# To change the output record separator, use the 'end' argument
-"""
-print(x, y, end="\n")
-print(x, y, sep="\t\t ", end="\n")
+#Positional Arguments: Passed in order of parameters.
+def print_numbers(a, b, c):
+    print(a, b, c)
+print_numbers(1, 2, 3)
+#Keyword Arguments: Passed using parameter names.
+def print_numbers(a, b, c):
+    print(a, b, c)
+print_numbers(a=1, c=3, b=2)
+#
