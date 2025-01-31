22fe5d305e8da8ce5a434cfcd92371f2e3cefe8c
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 10:49:55 +0530

    Create pp5.py

---
+++ lab 2/pp5.py
@@ -0,0 +1,7 @@
+print("Hello, world!")  # Basic usage:
+#
+name = "Alice"
+age = 30
+print("Name:", name, "Age:", age)  # Printing multiple values:
+#
+print(f"My name is {name} and I am {age} years old.")
