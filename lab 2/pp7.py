5cd0ffa5b1e609679c5c433018f492b28b6f154c
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 11:09:32 +0530

    Update pp7.py

--- lab 2/pp7.py
+++ lab 2/pp7.py
@@ -1,4 +1,11 @@
-var = 12 # integer
-var = 12.45 # float
-a = b = 2 # multiple assignment
-i_am = True # bool
+def outer_func():
+    b = 20 # Local to outer_func
+    print("b =", b)
+    def inner_func():
+        c = 10 # Local to inner_func
+        print("c =", c)
+        print("a in innerloop =",a)
+    inner_func()
+a = 1 # Global variable
+print("a =", a)
+outer_func()
