83b60a778e3ab5290719d571b8b147f28aa0e1c6
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 10:52:38 +0530

    Create pp10.py

---
+++ lab 2/pp10.py
@@ -0,0 +1,8 @@
+print("hello".upper() ) # Output: 'HELLO'
+print("HELLO".lower()  )# Output: 'hello'
+print(" hello ".strip() ) # Output: 'hello'
+print("apples".replace("a", "o"))  # Output: 'opples'
+# Checking Methods:
+s = "Python Programming"
+print(s.startswith("Python"))  # Output: True
+print(s.endswith("ing"))  # Output: True
