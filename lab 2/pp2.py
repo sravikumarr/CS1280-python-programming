ad62d90be695f53fb0da3fb82b0220583ca669fc
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 11:06:16 +0530

    Update pp2.py

--- lab 2/pp2.py
+++ lab 2/pp2.py
@@ -1,9 +1,7 @@
-# Input two numbers
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-# Calculate sum and average
-sum = num1 + num2
-average = sum / 2
-# Display results
-print(f"Sum of the two numbers is: {sum}")
-print(f"Average of the two numbers is: {average}")
+def outer_func():
+   global a
+   a=20
+   print("a =",a)
+a=10
+outer_func()
+print("global value of a=",a)
