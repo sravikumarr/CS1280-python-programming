42dc83c32518fc06706c3f73ac0db7cffdcf02fc
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 10:52:08 +0530

    Create pp9.py

---
+++ lab 2/pp9.py
@@ -0,0 +1,7 @@
+name = "Alice"
+age = 25
+pi = 22/7
+print("My name is %s and I am %d years old." % (name, age))
+print("My name is {} and I am {} years old.".format(name, age))
+print(f"My name is {name} and I am {age} years old.")
+print(f"Pi rounded to 2 decimals: {pi:.2f}")
