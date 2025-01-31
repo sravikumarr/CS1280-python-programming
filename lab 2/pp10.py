9ef6fc0aa7c517ac21cd0bf12795b0e1381b9cc5
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 11:10:32 +0530

    Delete lab 2/pp10.py

--- lab 2/pp10.py
+++
@@ -1,8 +0,0 @@
-print("hello".upper() ) # Output: 'HELLO'
-print("HELLO".lower()  )# Output: 'hello'
-print(" hello ".strip() ) # Output: 'hello'
-print("apples".replace("a", "o"))  # Output: 'opples'
-# Checking Methods:
-s = "Python Programming"
-print(s.startswith("Python"))  # Output: True
-print(s.endswith("ing"))  # Output: True
