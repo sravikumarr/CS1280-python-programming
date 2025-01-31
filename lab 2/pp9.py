12e0701118485ac6973084102738d705a8349a13
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-01-31 11:10:21 +0530

    Update pp9.py

--- lab 2/pp9.py
+++ lab 2/pp9.py
@@ -1,7 +1,11 @@
-name = "Alice"
-age = 25
-pi = 22/7
-print("My name is %s and I am %d years old." % (name, age))
-print("My name is {} and I am {} years old.".format(name, age))
-print(f"My name is {name} and I am {age} years old.")
-print(f"Pi rounded to 2 decimals: {pi:.2f}")
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result *= i
+        return result
+
+num  = int(input("Enter a number: "))
+print(f"Factorial of {num} is {factorial(num)}")
