c321a103a1f669f6b1ad0cf8c52374fe13b18eb4
Sumukh Ravikumar <sumuk.r21@gmail.com>
2025-02-19 14:21:54 +0530

    Create pp3.py

---
+++ Lab 4/pp3.py
@@ -0,0 +1,16 @@
+BCA ={
+  "USN":"1RUA24BCA0086",
+  "Name":"Sumukh R"
+}
+#CALLING THE KEY VALUE
+print("Here are the keys")
+for key in BCA:
+    print (key)
+#CALLING THE VALUES
+print("Here are the values")
+for key in BCA:
+    print(BCA[key])
+#HERE ARE THE KEYS IN VALUES
+for key,values in BCA.items():
+    print(key,values)
+print()
